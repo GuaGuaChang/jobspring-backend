ae2cd8984614567fafa93939a9c826b22ff51257
LittleLC <202003340112@zjut.edu.cn>
2025-09-02 20:28:42 +0800

    init backend

---
+++ src/test/java/com/jobspring/jobspringbackend/JobspringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobspring.jobspringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobspringBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
