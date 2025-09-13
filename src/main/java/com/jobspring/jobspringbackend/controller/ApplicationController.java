2eaf0782b542fad367b6a5b610c297f80d792e05
XinxingHu280311516 <E1288029@u.nus.edu>
2025-09-13 20:32:40 +0800

    application feature

--- src/main/java/com/jobspring/jobspringbackend/controller/ApplicationController.java
+++ src/main/java/com/jobspring/jobspringbackend/controller/ApplicationController.java
@@ -1,9 +1,35 @@
 package com.jobspring.jobspringbackend.controller;
 
+import com.jobspring.jobspringbackend.dto.ApplicationDTO;
+import com.jobspring.jobspringbackend.service.ApplicationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/applications")
+@RequiredArgsConstructor
 public class ApplicationController {
     // TODO: Application related endpoints
+
+    private ApplicationService applicationService;
+
+
+    @PostMapping(value = "/{jobId}/applications", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+//  @PreAuthorize("hasRole('CANDIDATE')")
+    public ResponseEntity<Void> apply(@PathVariable Long jobId, @ModelAttribute ApplicationDTO form,
+                                          @RequestPart(name = "file", required = false) MultipartFile file, Authentication auth)
+    {
+
+            Long userId = Long.valueOf(auth.getName());
+            Long id = applicationService.apply(jobId, userId, form, file);
+            return ResponseEntity.created(URI.create("/api/applications/" + id)).build();
+    }
+
 }
