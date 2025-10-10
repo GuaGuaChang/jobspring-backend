ad1b0e7dd10ac480732fd1e948d0eef849d59829
e1133132 <e1133132@u.nus.edu>
2025-10-10 21:05:54 +0800

    apply observer design pattern

--- src/main/java/com/jobspring/jobspringbackend/service/JobService.java
+++ src/main/java/com/jobspring/jobspringbackend/service/JobService.java
@@ -13,10 +13,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.context.ApplicationEventPublisher;
+
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Objects;
 
 @Service
 public class JobService {
